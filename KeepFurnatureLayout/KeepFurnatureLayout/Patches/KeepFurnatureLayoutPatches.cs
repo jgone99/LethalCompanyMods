7d551912593b558ae29e865f8ffb0ae31051f0a0
joshh <joshuahinojognzlz@gmail.com>
2024-01-16 14:28:36 -0500

    Updated KeepFurnatureLayout

--- KeepFurnatureLayout/KeepFurnatureLayout/Patches/KeepFurnatureLayoutPatches.cs
+++ KeepFurnatureLayout/KeepFurnatureLayout/Patches/KeepFurnatureLayoutPatches.cs
@@ -10,19 +10,12 @@ namespace KeepFurnatureLayout.Patches
 {
     internal class KeepFurnatureLayoutPatches
     {
-        static List<Tuple<int, Vector3, Vector3>> shipItems = new List<Tuple<int, Vector3, Vector3>> ();
-        static Tuple<int, Vector3, Vector3> cupboardInfo;
-        static GameObject cupboard;
-        static Vector3 parent2Pos;
-        static Vector3 parent2Rot;
-        static Vector3 parentPos;
-        static Vector3 parentRot;
+        static List<Tuple<int, Vector3, Vector3, Vector3, Vector3>> shipItems = new List<Tuple<int, Vector3, Vector3, Vector3, Vector3>> ();
 
         [HarmonyPatch(typeof(StartOfRound), "Awake"), HarmonyPostfix]
         static void GenerateConfigFile()
         {
             Plugin.config.SetupConfig(StartOfRound.Instance.unlockablesList.unlockables.ToArray());
-            cupboard = GameObject.Find("StorageCloset");
         }
 
         [HarmonyPatch(typeof(StartOfRound), "playersFiredGameOver"), HarmonyPrefix]
@@ -34,29 +27,49 @@ namespace KeepFurnatureLayout.Patches
             Plugin.log.LogInfo(unlockables.Count);
             for (int i = 0; i < unlockables.Count; i++)
             {
-                Plugin.log.LogInfo($"{unlockables[i].unlockableName} - unlocked({unlockables[i].hasBeenUnlockedByPlayer}) - Save({Plugin.config.configEntries[i].Value})");
-                if (unlockables[i].hasBeenUnlockedByPlayer && Plugin.config.configEntries[i].Value)
+                Plugin.log.LogInfo($"{unlockables[i].unlockableName} - Type({unlockables[i].unlockableType}) - placeable({unlockables[i].IsPlaceable})" +
+                    $" - playerUnlocked({unlockables[i].hasBeenUnlockedByPlayer}) - Save({Plugin.config.configEntries[i].Value})");
+                if (Plugin.config.configEntries[i].Value)
                 {
-                    shipItems.Add(Tuple.Create(i, unlockables[i].placedPosition, unlockables[i].placedRotation));
+                    if (unlockables[i].hasBeenUnlockedByPlayer || unlockables[i].alreadyUnlocked)
+                    {
+                        Vector3 parentRot = Vector3.zero;
+                        Vector3 parentPos = Vector3.zero;
+
+                        if (unlockables[i].unlockableType == 1 && unlockables[i].IsPlaceable)
+                        {
+                            PlaceableShipObject objectToSave;
+                            if (unlockables[i].alreadyUnlocked)
+                            {
+                                Plugin.log.LogInfo("Already unlocked");
+                                objectToSave = UnityEngine.Object.FindObjectsOfType<PlaceableShipObject>().FirstOrDefault(item => item.unlockableID == i);
+                                Plugin.log.LogInfo($"Count: {UnityEngine.Object.FindObjectsOfType<PlaceableShipObject>().Where(item => item.unlockableID == i).Count()}");
+                                StartOfRound.Instance.SpawnedShipUnlockables.TryAdd(i, objectToSave.parentObject.gameObject);
+                            }
+                            else
+                            {
+                                Plugin.log.LogInfo("Player unlocked");
+                                Plugin.log.LogInfo(unlockables[i].prefabObject.name);
+                                objectToSave = StartOfRound.Instance.SpawnedShipUnlockables.GetValueOrDefault(i).GetComponentInChildren<PlaceableShipObject>();
+                            }
+
+                            if (objectToSave.parentObjectSecondary != null)
+                            {
+                                parentRot = objectToSave.parentObjectSecondary.transform.eulerAngles;
+                                parentPos = objectToSave.parentObjectSecondary.position;
+                            }
+                            else if (objectToSave.parentObject != null)
+                            {
+                                parentRot = objectToSave.parentObject.rotationOffset;
+                                parentPos = objectToSave.parentObject.positionOffset;
+                            }
+                        }
+
+                        Plugin.log.LogInfo($"{unlockables[i].unlockableName} saved");
+                        shipItems.Add(Tuple.Create(i, unlockables[i].placedPosition, unlockables[i].placedRotation, parentRot, parentPos));
+                    }
                 }
             }
-
-            PlaceableShipObject placeableShipObject = cupboard.GetComponentInChildren<PlaceableShipObject>();
-
-            if (placeableShipObject.parentObjectSecondary != null)
-            {
-                parent2Rot = placeableShipObject.parentObjectSecondary.transform.eulerAngles;
-                parent2Pos = placeableShipObject.parentObjectSecondary.position;
-            }
-            else if (placeableShipObject.parentObject != null)
-            {
-                parentRot = placeableShipObject.parentObject.rotationOffset;
-                parentPos = placeableShipObject.parentObject.positionOffset;
-            }
-
-            int cupboardID = cupboard.GetComponentInChildren<PlaceableShipObject>().unlockableID;
-            Plugin.log.LogInfo($"{unlockables[cupboardID].unlockableName} - Save({Plugin.config.configEntries[cupboardID].Value})");
-            cupboardInfo = Tuple.Create(cupboardID, unlockables[cupboardID].placedPosition, unlockables[cupboardID].placedRotation);
         }
 
         [HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.ResetShip)), HarmonyPostfix]
@@ -69,40 +82,55 @@ namespace KeepFurnatureLayout.Patches
                 return;
             }
 
+/*            StartOfRound.Instance.closetLeftDoor.TriggerAnimationNonPlayer();
+            StartOfRound.Instance.closetRightDoor.TriggerAnimationNonPlayer();*/
+
             UnlockableItem[] unlockables = StartOfRound.Instance.unlockablesList.unlockables.ToArray();
-            foreach (Tuple<int, Vector3, Vector3> item in shipItems)
+            foreach (Tuple<int, Vector3, Vector3, Vector3, Vector3> item in shipItems)
             {
-                StartOfRound.Instance.BuyShipUnlockableServerRpc(item.Item1, TimeOfDay.Instance.quotaVariables.startingCredits);
-                if (unlockables[item.Item1].unlockableType == 1)
-                {
-                    Plugin.log.LogInfo($"Placing: {unlockables[item.Item1].unlockableName}");
-                    GameObject placingObject = StartOfRound.Instance.SpawnedShipUnlockables.GetValueOrDefault(item.Item1);
-                    ShipBuildModeManager.Instance.PlaceShipObject(item.Item2, item.Item3, placingObject.GetComponentInChildren<PlaceableShipObject>(), placementSFX: false);
-                    ShipBuildModeManager.Instance.PlaceShipObjectServerRpc(item.Item2, item.Item3, placingObject.GetComponent<NetworkObject>(), (int)GameNetworkManager.Instance.localPlayerController.playerClientId);
+                Plugin.log.LogInfo($"Next item: {unlockables[item.Item1].unlockableName}");
+
+                if (unlockables[item.Item1].IsPlaceable)
+                {   
+                    if (unlockables[item.Item1].alreadyUnlocked)
+                    {
+                        Plugin.log.LogInfo("Already unlocked");
+                        PlaceableShipObject objectToRestore = StartOfRound.Instance.SpawnedShipUnlockables.GetValueOrDefault(item.Item1).GetComponentInChildren<PlaceableShipObject>();
+                        
+
+                        if (objectToRestore.parentObjectSecondary != null)
+                        {
+                            objectToRestore.parentObjectSecondary.transform.eulerAngles = item.Item4;
+                            objectToRestore.parentObjectSecondary.position = item.Item5;
+                        }
+                        else if (objectToRestore.parentObject != null)
+                        {
+                            objectToRestore.parentObject.rotationOffset = item.Item4;
+                            objectToRestore.parentObject.positionOffset = item.Item5;
+                        }
+                    }
+                    else
+                    {
+                        Plugin.log.LogInfo("Player unlocked");
+                        Plugin.log.LogInfo($"Buying: {unlockables[item.Item1].unlockableName}");
+                        StartOfRound.Instance.BuyShipUnlockableServerRpc(item.Item1, TimeOfDay.Instance.quotaVariables.startingCredits);
+                    }
+                    
+                    if (unlockables[item.Item1].unlockableType == 1)
+                    {
+                        Plugin.log.LogInfo($"Placing: {unlockables[item.Item1].unlockableName}");
+                        GameObject placingObject = StartOfRound.Instance.SpawnedShipUnlockables.GetValueOrDefault(item.Item1);
+                        Plugin.log.LogInfo($"IsSpawned: {placingObject.GetComponent<NetworkObject>().IsSpawned}");
+                        ShipBuildModeManager.Instance.PlaceShipObject(item.Item2, item.Item3, placingObject.GetComponentInChildren<PlaceableShipObject>(), placementSFX: false);
+                        ShipBuildModeManager.Instance.PlaceShipObjectServerRpc(item.Item2, item.Item3, placingObject.GetComponent<NetworkObject>(), (int)GameNetworkManager.Instance.localPlayerController.playerClientId);
+                    }
                 }
                 else
                 {
-                    Plugin.log.LogInfo("Item is suit; not placing");
+                    Plugin.log.LogInfo($"Buying: {unlockables[item.Item1].unlockableName}");
+                    StartOfRound.Instance.BuyShipUnlockableServerRpc(item.Item1, TimeOfDay.Instance.quotaVariables.startingCredits);
                 }
             }
-
-            PlaceableShipObject placeableShipObject = cupboard.GetComponentInChildren<PlaceableShipObject>();
-
-            if (placeableShipObject.parentObjectSecondary != null)
-            {
-                placeableShipObject.parentObjectSecondary.transform.eulerAngles = parent2Rot;
-                placeableShipObject.parentObjectSecondary.position = parent2Pos;
-            }
-            else if (placeableShipObject.parentObject != null)
-            {
-                placeableShipObject.parentObject.rotationOffset = parentRot;
-                placeableShipObject.parentObject.positionOffset = parentPos;
-            }
-
-            Plugin.log.LogInfo($"Placing: {unlockables[cupboardInfo.Item1].unlockableName}");
-            ShipBuildModeManager.Instance.PlaceShipObject(cupboardInfo.Item2, cupboardInfo.Item3, cupboard.GetComponentInChildren<PlaceableShipObject>(), placementSFX: false);
-            ShipBuildModeManager.Instance.PlaceShipObjectServerRpc(cupboardInfo.Item2, cupboardInfo.Item3, cupboard.GetComponent<NetworkObject>(), (int)GameNetworkManager.Instance.localPlayerController.playerClientId);
-            Plugin.log.LogInfo($"Count: {UnityEngine.Object.FindObjectsOfType<PlaceableShipObject>().Where(item => item.unlockableID == cupboardInfo.Item1).Count()}");
         }
     }
 }
--- KeepFurnatureLayout/KeepFurnatureLayout/obj/Debug/netstandard2.1/KeepFurnatureLayout.AssemblyInfo.cs
+++ KeepFurnatureLayout/KeepFurnatureLayout/obj/Debug/netstandard2.1/KeepFurnatureLayout.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KeepFurnatureLayout")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20dc577e0c397b3ef93a1cb6a70ab15b21deaf0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("KeepFurnatureLayout")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KeepFurnatureLayout")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
